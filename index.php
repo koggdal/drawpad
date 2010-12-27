6114c7c1c39eac7ea47cce35704af7a5c40b5a01
Johannes Koggdal <johannes@koggdal.com>
2010-12-27 20:37:44 +0100

    Updated base path to localhost

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>DrawApp</title>
-	<base href="http://drawapp.koggdal.com/" />
+	<base href="http://localhost/drawapp/" />
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;" />
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
